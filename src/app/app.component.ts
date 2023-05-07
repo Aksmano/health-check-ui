6cd5b3c925c9d61b6634480b19a966dfeed27939
Aksmano <aksman.bartlomiej@gmail.com>
2023-05-07 18:15:37 +0200

    Added static user info fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { fetchCitiesRequest } from './redux/actions/medical-offer.actions';
 import { AppState } from './redux/index.reducers';
 import { loadKeycloakInfoRequest } from './redux/actions/user-info-actions/keylcloak-info.actions';
 import { KeycloakService } from 'keycloak-angular';
+import { UserInfo } from './core/user-info';
+import { DoctorServiceImpl } from './data/services/doctor/doctor.service';
+import { AdministrationServiceImpl } from './data/services/administration/administration.service';
+import { PatientService } from './data/services/patient/patient.service';
+import { ReceptionistService } from './data/services/receptionist/receptionist.service';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +17,26 @@ import { KeycloakService } from 'keycloak-angular';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public userInfo: UserInfo;
+  public title: string = 'health-check-ui';
+
   constructor(
     private readonly primengConfig: PrimeNGConfig,
-    private readonly store: Store<AppState>
-  ) { }
+    private readonly store: Store<AppState>,
+    private readonly doctorService: DoctorServiceImpl,
+    private readonly adminService: AdministrationServiceImpl,
+    private readonly patientService: PatientService,
+    private readonly receptionistService: ReceptionistService,
+    private readonly keycloak: KeycloakService
+  ) {
+    this.userInfo = new UserInfo(doctorService, adminService, patientService, receptionistService, keycloak);
+  }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
 
-    this.store.dispatch(fetchCitiesRequest());
-    this.store.dispatch(loadKeycloakInfoRequest());
+
+    // this.store.dispatch(fetchCitiesRequest());
+    // this.store.dispatch(loadKeycloakInfoRequest());
   }
 }
